c32c62972f333f738604c3ea250ad9addcb7c34d
Justin Tompkins <jtompkins9045@gmail.com>
2021-11-07 22:54:23 -0500

    hey

---
+++ script.js
@@ -0,0 +1,5 @@
+let btnPrimary = document.createElement('btnPrimary');
+btnPrimary.addEventListener('click', nextPage);
+    button.innerText = 'Another Please';
+    button.className = 'reset';
+    button.style = 'margin: 0; margin-top: 4em;'
